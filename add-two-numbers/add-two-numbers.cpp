b25957873e49d39c2ba3d8da4769003f288027e0
LT <56750861+tao-luke@users.noreply.github.com>
2021-04-08 00:10:40 -0400

    Time: 28 ms (62.01%), Space: 70.7 MB (93.72%) - LeetHub

--- add-two-numbers/add-two-numbers.cpp
+++ add-two-numbers/add-two-numbers.cpp
@@ -15,37 +15,40 @@ public:
         if (l2 == nullptr) return l1;
         bool carry = false;
         ListNode* result = l1;
-        int tmp = 0;
         while(!(l1 == nullptr && l2 == nullptr) || carry){
             if (l1 != nullptr && l2 != nullptr && l1->val != -1){ //normal case
-                tmp = l1->val + l2->val;
-                if (carry) tmp++;
-                if (tmp < 10) carry = false;
-                else carry = true;
-                l1->val = tmp%10;
+                l1->val = l1->val + l2->val;
+                if (carry){
+                    l1->val++;
+                    if (l1->val < 10) carry = false;
+                }
+                if (l1->val >= 10) carry = true;
+                l1->val = l1->val%10;
                 if (l1->next == nullptr &&(carry || l2->next != nullptr)) l1->next = new ListNode(-1);
                 l1 = l1->next;
                 l2 = l2->next;
             }else if (l1->val == -1){
-                tmp = 0;
+                l1->val = 0;
                 if (l2 != nullptr){
-                    tmp = l2->val;
+                    l1->val = l2->val;
                     l2 = l2->next;
                 }
                 if (carry){
-                    tmp++;
+                    l1->val++;
+                    if (l1->val < 10) carry = false;
                 }
-                if (tmp < 10) carry = false;
-                else carry = true;
-                l1->val = tmp%10;
+                
+                if (l1->val >= 10) carry = true;
+                l1->val = l1->val%10;
                 if (l1->next == nullptr &&(carry || l2 != nullptr)) l1->next = new ListNode(-1);
                 l1 = l1->next;
             }else{
                 if (carry){
                     l1->val++;
+                    if (l1->val < 10) carry = false;
                 }
-                if (l1->val < 10) carry = false;
-                else carry = true;
+                
+                if (l1->val >= 10) carry = true;
                 l1->val = l1->val%10;
                 if (l1->next == nullptr && carry){
                     l1->next = new ListNode(-1);
