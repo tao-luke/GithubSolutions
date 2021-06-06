c911386eae210a8354076c06e10def6321628dfa
LT <56750861+tao-luke@users.noreply.github.com>
2021-06-06 18:18:55 -0400

    Time: 8 ms (92.57%), Space: 10.5 MB (68.92%) - LeetHub

--- odd-even-linked-list/odd-even-linked-list.cpp
+++ odd-even-linked-list/odd-even-linked-list.cpp
@@ -12,31 +12,27 @@ class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
         if (head == nullptr || head->next == nullptr) return head;
-        ListNode* oddStart = head;
         ListNode* evenStart = head->next;
-        ListNode* oddNext = oddStart;
+        ListNode* oddNext = head;
         ListNode* evenNext = evenStart;
         ListNode* current = head->next;
         
         int counter = 2;
         while(current != nullptr){
-            if (counter <=2){
-                counter++;
-                current = current->next;
-                continue;
-            } 
-            if (counter %2 == 0){
+            if (counter > 2){
+                if (counter %2 == 0){
                 evenNext->next = current;
                 evenNext = current;
-            }else{
-                oddNext->next = current;
-                oddNext = current;
-            }
+                }else{
+                    oddNext->next = current;
+                    oddNext = current;
+                }
+            } 
             current = current->next;
             counter++;
         }
         evenNext->next = nullptr;
         oddNext->next = evenStart;
-        return oddStart;
+        return head;
     }
 };
