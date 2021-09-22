3a9aee8349d786919dd0a83094ea07d5bb839dc1
LT <56750861+tao-luke@users.noreply.github.com>
2021-09-22 13:11:10 -0400

    Time: 120 ms (42.59%), Space: 20 MB (87.51%) - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
+    // the idea is just like 2sum but we lock off 1 index at a time.ofc we sort first
+    //suppose arr [a,b,c,d,e], we would go through the array with a locked, then b locked, then c and so on. With each time, having the other 2 inedx to the right of the locked one and adjusting as needed. Ex: a locked, j is on b and k is on e, then we calc the sum of a b e, if big, shift ptr to e to d and so on.
+    // we get dups however if naively impelent like above, we thus elminate them by each time scoring a j,k that we update j and k to the next diff value. same logic applies to each time we up i, keep upating it till its a diff value.
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> result{};
         short size = nums.size();
-        sort(nums.begin(),nums.end());
+        heapSort(nums);
+        //sort(nums.begin(),nums.end());
         for(short i = 0; i <size;i++){
             if (i > 0 && nums[i] == nums[i-1])continue; //dup n1
             short j = i+1;
@@ -11,12 +15,18 @@ public:
             while(j < k){
                 if (nums[j]+nums[k] == -nums[i]){
                     result.push_back(vector<int>{nums[i],nums[j],nums[k]});
-                    while(j < k && nums[k] == nums[k-1])k--;
-                    while(j < k && nums[j]  == nums[j+1])j++;
+                    while(j < k && nums[k] == nums[k-1])k--; //dup3
+                    while(j < k && nums[j]  == nums[j+1])j++; //dup2
                     k--;j++;
                 } 
-                else if (nums[j]+nums[k] < -nums[i]) j++;
-                else k--;
+                else if (nums[j]+nums[k] < -nums[i]){
+                    while(j < k && nums[j]  == nums[j+1])j++; //dup2
+                    j++;
+                }
+                else{
+                    while(j < k && nums[k] == nums[k-1])k--; //dup3
+                    k--;
+                }
             }
         }
         return result;
