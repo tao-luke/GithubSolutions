9192f8fa81bb7752c9d4b648b07983b4feefae05
LT <56750861+tao-luke@users.noreply.github.com>
2021-10-05 13:19:15 -0400

    Time: 0 ms (100.00%), Space: 7.8 MB (99.59%) - LeetHub

--- binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,16 +11,43 @@
  */
 class Solution {
 public:
-    //recursion:
-    void helper(vector<int>& result, TreeNode* root){
-        if (root == nullptr) return;
-        helper(result,root->left);
-        result.push_back(root->val);
-        helper(result,root->right);
+    TreeNode* findRightMost(TreeNode* root){
+        while(root->right){
+            root = root->right;
+        }
+        return root;
     }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> result{};
-        helper(result,root);
+        if (!root) return result;
+        while(root){
+            if (root->left){
+                findRightMost(root->left)->right = root;
+                TreeNode* tmp = root->left;
+                root->left = nullptr;
+                root = tmp;
+            }else{
+                result.push_back(root->val);
+                root = root->right;
+            }
+        }
         return result;
+//         vector<int> result{};
+//         if (root == nullptr) return result;
+//         stack<TreeNode*> hist{};
+//         hist.push(root);
+//         while(!hist.empty()){
+//             root = hist.top();
+//             if (root->left == nullptr){
+//                 result.push_back(root->val);
+//                 hist.pop();
+//                 if (root->right) hist.push(root->right);
+//             }else{
+//                 hist.push(root->left);
+//                 root->left = nullptr;
+//             }
+            
+//         }
+//         return result;
     }
 };
