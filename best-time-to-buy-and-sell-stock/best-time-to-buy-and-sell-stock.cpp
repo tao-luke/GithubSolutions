338349241965206cf542e46fffe0ad4956dba878
LT <56750861+tao-luke@users.noreply.github.com>
2021-11-29 16:20:42 -0500

    Time: 104 ms (94.16%), Space: 93.3 MB (52.85%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        unsigned short buy = USHRT_MAX;
-        unsigned short profit = 0;
-        for(const auto&n: prices){
-            if (n < buy){
-                buy = n;
-            }else{
-                if ((n - buy) > profit) profit = n-buy;
-            }
+        //In short, we have 2 states. 
+        //  a) we have a stock dp[1][i] = dp[1][i-1] or dp[0][i-1] - stock
+        //  b) we do NOT have a stock dp[0][i] = dp[0][i-1] or dp[1][i-1] + stock
+        // but since we can only do one buy and sell, we can just keep track of a min and profit thus
+        int low = prices[0];
+        int profit = 0;
+        for(const auto&n: prices) {
+            profit = max(profit,n - low);
+            low = min(low, n);
         }
         return profit;
     }
