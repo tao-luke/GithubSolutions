fc2d94f299e7a69fb6ae96de444dbb4450cdfaa7
LT <56750861+tao-luke@users.noreply.github.com>
2021-12-01 15:20:59 -0500

    Time: 0 ms (100.00%), Space: 11.3 MB (47.38%) - LeetHub

--- best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        //for this kind of problem, let us truly divide down the issue.
-        // we have 2 states in owning a share, yes or no
-        // we have unlimited transactions
-        // we have prices.size() days of records to check
-        // thus define: p[i][j] = max profit on day i with j (0<=j<=1) shares
-        // the recursive rule should be: p[i][0] = max(
-        //                                              p[i-1][0] //no share just like yesterday, 
-        //                                              p[i-1][1]+prices[i] //had one yesterday and sold today
-        //                                             )
-        // or                           p[i][1] = max(p[i-1][1],p[i-2][0]-prices[i]) //if buy today, yesterday is BANNEd from use
-        short size = prices.size();
-        if (size < 2) return 0;
-        int p_i_0 = 0; //day 1 and 0 own, 0 profit
-        int p_i_1 = -prices[0]; //day 1 and 1 own, then bought day 1
-        int tmp = 0; // impossible
-        int tmp1 = 0;
-        for(short i = 1;i < size;i++){
-            tmp1 = p_i_0;
-            p_i_0 = max(p_i_0,p_i_1+prices[i]);
-            p_i_1 = max(p_i_1,tmp-prices[i]);
-            tmp = max(tmp,tmp1);
+        //let dp[k][1][i] = the most profit with k+1 buys and with one stock owned on day i
+        //                = max(dp[k][1][i-1],dp[k-1][0][i-2]-prices[i]) //we cant use the max from yesterday, cause its blocked.
+        //then dp[k][0][i] = max(dp[k][0][i-1],dp[k][1][i-1]+prices[i])
+        // then in to calculate dp[k][0][i], we need dp[k-1][0][i-2] and dp[k][1][i-1]
+        //however, we dont have a k limit
+        //then dp[1][i] = max(dp[1][i-1],dp[0][i-2]-prices[i]);
+        //     dp[0][i] = max(dp[0][i-1],dp[1][i-1]+prices[i]);
+        int dp[2][prices.size()];
+        dp[0][0] = 0;
+        dp[1][0] = -prices[0];
+        for(int i = 1; i < prices.size();i++) {
+            dp[1][i] = max(dp[1][i-1],dp[0][max(0,i-2)]-prices[i]);
+            dp[0][i] = max(dp[0][i-1],dp[1][i-1]+prices[i]);
         }
-        return p_i_0;
+        return dp[0][prices.size()-1];
     }
 };
