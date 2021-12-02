5d32c91989e6ecf0dfef47b5e2f82ffdaa305c91
LT <56750861+tao-luke@users.noreply.github.com>
2021-12-02 01:03:55 -0500

    Time: 60 ms (88.92%), Space: 9 MB (99.43%) - LeetHub

--- partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        // th dp here to ask is if we can us a set of number to reach a target sum.
-        unsigned char size = nums.size();
-        if (size ==1) return false;
         int target = 0;
         for(const auto&n: nums){
             target+=n;
         }
         if (target % 2 != 0) return false;
-        target /=2;
-        vector<bool> dp(target+1, false);
-        dp[0] = true;
-        for(const auto& n: nums){
-            for(int sum = target; sum > 0; sum--){
-                if (sum >= n){
-                    dp[sum] = dp[sum] || dp[sum - n];
-                }
-            }
+        target/=2;
+        bool dp[target+1];
+        for(int i = 0; i < target+1;i++){
+            dp[i]= false;
         }
-        return dp[target];
+        dp[0] = true;
+        for(int i = 0; i < nums.size(); i++){
+            for(int w = target; w>= nums[i]; w--){
+                dp[w] = dp[w] or (dp[w-nums[i]]);
+            }
+            // for(int j = 0;j < target+1;j++){
+            //     cout << dp[j] << " ";
+            // }
+            // cout << endl;
+            if (dp[target]) return true;
+        }
+        return false;
     }
 };
