836ed8bcae7edce002167dcf04a76f74de15641c
LT <56750861+tao-luke@users.noreply.github.com>
2022-02-11 20:23:55 -0500

    Time: 29 ms (23.75%), Space: 7.2 MB (14.13%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+        //sliding window. keep r and l. the goal is to keep increasing r each iteration. and after each iteration, we update l accordingly. while we keep some invariant in the window. In this case, we note the size of the window - most occuring char in the window must be no MORE than k. so keep increasing r. each time, if the invariant fails, we keep increasing l and update our most occurding char.
         int l = 0;
         int r = 0;
         int result = 0;
