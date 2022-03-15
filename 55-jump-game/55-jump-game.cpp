8600f821449a9bb56538df5351e84cbe88af26e6
LT <56750861+tao-luke@users.noreply.github.com>
2022-03-14 23:03:01 -0400

    Time: 92 ms (58.24%), Space: 48.5 MB (31.69%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int nearest_end = nums.size()-1;
-        for(int i = nums.size()-2;i >= 0;i--){
-            if (i + nums[i] >= nearest_end) nearest_end = i;
+        int lastIndex = nums.size()-1;
+        for(int i = nums.size()-1;i >=0;i--){
+            if (nums[i]+i >= lastIndex) lastIndex = i;
         }
-        if (nearest_end == 0) return true;
-        return false;
+        return lastIndex == 0;
     }
 };
