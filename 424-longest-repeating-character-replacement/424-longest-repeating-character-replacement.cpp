74bf91007ebd5157918cc84eb7c6251de224709c
LT <56750861+tao-luke@users.noreply.github.com>
2022-04-12 15:47:45 -0400

    Time: 42 ms (14.05%), Space: 7 MB (84.23%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        //sliding window. keep r and l. the goal is to keep increasing r each iteration. and after each iteration, we update l accordingly. while we keep some invariant in the window. In this case, we note the size of the window - most occuring char in the window must be no MORE than k. so keep increasing r. each time, if the invariant fails, we keep increasing l and update our most occurding char.
         int l = 0;
         int r = 0;
-        int result = 0;
-        int local_count = 0;
-        unordered_map<char,int> hist{};
-        for(;r < s.size();r++){
-            hist[s[r]]++;
-            if (hist[s[r]] > local_count) local_count = hist[s[r]];
-            while(r - l + 1 - local_count > k){
-                hist[s[l]]--;
+        int ans = 0;
+        int size = 0;
+        unordered_map<char,int> seen{}; //char to count mapping
+        int dom_count = 0;
+        while (r < s.size()){
+            char c = s[r];
+            seen[c]++;
+            dom_count = max(dom_count, seen[c]);
+            size++;
+            while(size - dom_count > k){
+                seen[s[l]]--;
+                size--;
                 l++;
-                for(const auto& p: hist){
-                    local_count = max(local_count, p.second);
-                }
             }
+            ans = max(ans, size);
+            dom_count = 0;
+            for(const auto& p: seen){
+                dom_count = max(dom_count, p.second);
+            }
+            r++;
         }
-        return max(result, r-l);
+        return ans;
     }
 };
