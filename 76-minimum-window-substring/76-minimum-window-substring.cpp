0a90ea8f953df6546ec525353ec24c36b15e526d
LT <56750861+tao-luke@users.noreply.github.com>
2022-04-23 00:35:30 -0400

    Time: 23 ms (61.16%), Space: 16.3 MB (18.95%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char,int> hist{};
-        for(const auto& c: t){
-            hist[c]++;
-        }
-        int expect = t.size();
-        unordered_map<char,int> seen{};
         int l = 0;
         int r = 0;
-        string result = "";
-        int local = 0;
-        for(;r < s.size();r++){
-            if (hist.count(s[r]) != 0){
-                int tmp = seen[s[r]];
-                if (tmp < hist[s[r]]) local++;
-                seen[s[r]]++;
+        unordered_map<char, int> dict{};
+        for(const auto& c: t){
+            dict[c]++;
+        }
+        string result;
+        int rel_size = 0;
+        while(r < s.size()){
+            char c = s[r];
+            if (dict.count(c) != 0){
+                if (dict[c] > 0){
+                    rel_size++;
+                }
+                dict[c]--;
             }
-            while(local == expect){
-                if (result == ""|| r-l+1 < result.size()) result = s.substr(l,r-l+1);
-                if (hist.count(s[l]) != 0){
-
-                    if (seen[s[l]] <= hist[s[l]]){
-                        local--;
+            while(rel_size == t.size()){
+                if (result.size() == 0 || result.size() > r - l+ 1){
+                    result = s.substr(l,r-l+1);
+                }
+                c = s[l];
+                if (dict.count(c) != 0){
+                    if (dict[c] == 0){
+                        rel_size--;
                     }
-                    seen[s[l]]--;
+                    dict[c]++;
                 }
                 l++;
-                // cout <<l << "  " << r << endl;
             }
-            
+            r++;
+            // cout << l << "  " << r <<"   str:" << result << " rel size: " << rel_size << endl;
         }
         return result;
     }
