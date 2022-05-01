0482cd7276c5db1daa0c537b58eb380be22b540b
LT <56750861+tao-luke@users.noreply.github.com>
2022-05-01 14:25:07 -0400

    Time: 55 ms (11.84%), Space: 15.6 MB (15.98%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& e) {
-        unordered_map<int,vector<int>> edges{};
-        for(int i = 0; i < e.size();i++){
-            edges[e[i][0]].push_back(e[i][1]);
-            if (e[i][0] == e[i][1]) return false;
+    bool canFinish(int numCourses, vector<vector<int>>& prer) {
+        unordered_map<int,vector<int>> pre{};
+        for(const auto& v: prer){
+            pre[v.front()].push_back(v.back());
         }
-        vector<int> leaf(numCourses, 0);
-        vector<int> path(numCourses, 0);
+        unordered_set<int> have{};
+        unordered_set<int> visited{};
         for(int i = 0; i < numCourses; i++){
-            if (!try_Course(i, edges, leaf, path)) return false;
+            if (have.count(i) != 1){
+                if (!try_course(i,have,pre, visited)) return false;
+            }
         }
         return true;
     }
-    bool try_Course(int course, unordered_map<int, vector<int>>& edges, vector<int>& leaf, vector<int>& path){
-        if (path[course] == 1) return false;
-        if (leaf[course] == 1 || edges[course].empty()){
-            leaf[course] = 1;
+    bool try_course(int i, unordered_set<int>& have, const unordered_map<int,vector<int>>& pre, unordered_set<int>& visited){
+        if (have.count(i) == 1) return true;
+        if (pre.count(i) == 0){
+            have.insert(i);
             return true;
-        } 
-        path[course] = 1;
-        for(const auto& need: edges[course]){
-            if (!try_Course(need, edges, leaf, path)) return false;
         }
-        path[course] = 0;
-        leaf[course] = 1;
+        if (visited.count(i) == 1) return false;
+        visited.insert(i);
+        for(const auto& e: pre.find(i)->second){
+            if (!try_course(e,have,pre, visited)) return false;
+        }
+        //visited.erase(i);
+        have.insert(i);
         return true;
     }
 };
