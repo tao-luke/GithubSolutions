c1da5734f999122c68396b3daa11ea5748938b8e
LT <56750861+tao-luke@users.noreply.github.com>
2022-05-01 00:13:33 -0400

    Time: 365 ms (31.11%), Space: 7.7 MB (68.89%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        vector<vector<bool>> dp(3, vector<bool>(s.size(),1));
+        bool* dp0 = new bool[s.size()];
+        bool* dp1 = new bool[s.size()];
+        bool* dp2 = new bool[s.size()];
+        memset(dp0,1,s.size()*sizeof(bool));
+        
         int result_i = 0;
         int result_l = 1;
         for(int l = 2; l <= s.size(); l++){
             for(int i = 0; i <= s.size() - l; i++){
                 bool tmp = (s[i] == s[i+l-1]);
                 if (l != 2){
-                    tmp = tmp && dp[0][i+1];
+                    tmp = tmp && dp0[i+1];
                 }
-                dp[2][i] = tmp;
-                if (dp[2][i] && l > result_l){
+                dp2[i] = tmp;
+                if (dp2[i] && l > result_l){
                     result_i = i;
                     result_l = l;
                 }
-                // cout << "i: " << i << "  l:  " << l <<"  dp:" << dp[i] << endl;
+                //cout << "i: " << i << "  l:  " << l <<"  dp:" << dp2[i] << endl;
             }
             if (l != 2){
-                swap(dp[0],dp[1]);
-                swap(dp[1],dp[2]);
+                swap(dp0,dp1);
+                swap(dp1,dp2);
             }else{
-                swap(dp[1],dp[2]);
+                swap(dp1,dp2);
             }
         }
+        // delete dp0;
+        // delete dp1;
+        // delete dp2;
         return s.substr(result_i,result_l);
     }
 };
