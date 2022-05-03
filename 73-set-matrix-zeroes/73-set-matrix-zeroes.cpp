bffa8dfc2aee31cae4ba1a58334241ee04acdb64
LT <56750861+tao-luke@users.noreply.github.com>
2022-05-02 23:02:33 -0400

    Time: 34 ms (7.14%), Space: 13.2 MB (54.89%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        // consider the first row and column as axis:
+        bool row0_empty = false;
+        bool column0_empty = false;
+        for(int i = 0; i < matrix.size();i++){
+            if (matrix[i][0] == 0) {
+                column0_empty = true;
+                break;
+            }
+        }
+        for(int i = 0; i < matrix[0].size();i++){
+            if (matrix[0][i] == 0){
+                row0_empty = true;
+                break;
+            }
+        }
+        for(int i = 0; i < matrix.size();i++){
+            for(int j = 0; j < matrix[0].size();j++){
+                if (matrix[i][j] == 0){
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                } 
+            }
+        }
+        for(int i = 1; i < matrix.size();i++){
+            for(int j = 1; j < matrix[0].size();j++){
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) matrix[i][j] = 0;
+            }
+        }
+        if (column0_empty){
+            for(int i = 0; i < matrix.size();i++){
+                matrix[i][0] = 0;
+            }
+        }
+        if (row0_empty){
+            for(int i = 0; i < matrix[0].size();i++){
+                matrix[0][i] = 0;
+            }
+        }
+    }
+};
