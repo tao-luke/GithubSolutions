b8c49d8a1800437ecb14e8f3c996b29df6744007
LT <56750861+tao-luke@users.noreply.github.com>
2022-05-02 23:03:24 -0400

    Time: 13 ms (81.03%), Space: 13 MB (95.93%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         // consider the first row and column as axis:
+        // 1) check if 0 exist in either and save the response
+        // 2) go through each 
         bool row0_empty = false;
         bool column0_empty = false;
         for(int i = 0; i < matrix.size();i++){
@@ -16,8 +18,8 @@ public:
                 break;
             }
         }
-        for(int i = 0; i < matrix.size();i++){
-            for(int j = 0; j < matrix[0].size();j++){
+        for(int i = 1; i < matrix.size();i++){
+            for(int j = 1; j < matrix[0].size();j++){
                 if (matrix[i][j] == 0){
                     matrix[0][j] = 0;
                     matrix[i][0] = 0;
