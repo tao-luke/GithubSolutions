79e5ce51b7688dda6b43346b4de090eb7789d1e7
LT <56750861+tao-luke@users.noreply.github.com>
2022-05-18 19:40:03 -0400

    Time: 191 ms, Memory: 11.6 MB - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.cpp
+++ longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,41 +1,26 @@
 class Solution {
 public:
-    /* soln idea: 
-    /*       trial 1: attempted to use a left and right ptr to capture a palindrome and move forward.
-    /*       problem: palindrome could be anywhere and its not consistent with the window. Forexample, you cant just proclaim a length 3 string isnt going to be in a palindrome if it isnt. it come be a part of another.
-    /*       soln: find centers! the only thing consistent considering palindromes are the centers! try each center and expand around it. 
-    */
-    
-
-    short expandAt(const string& s, const short& l1, const short& l2){
-        short left = (l1 == l2)? (l1-1):(l1);
-        short right = (l1 == l2)? (l2+1):(l2);
-        short length = (l2 == l1)? (1):(0);
-        
-        while(left >= 0 && right <s.size() && s[left] == s[right]){
-            length+=2;
-            left--;
-            right++;
-        }
-        return length;
-    }
     string longestPalindrome(string s) {
-        short center = 0;
-        short length = 0;
-        for(short i = 0; i < s.size();i++){
-            short l1 = expandAt(s,i,i);
-            short l2 = expandAt(s,i,i+1);
-            if (max(l1,l2) > length){
-                length = max(l1,l2);
-                if (l1 > l2) center = i;
-                else center = (i+1) * -1;
+        int dp[s.size()+1][s.size()]; //dp[l][i] = true if s is palindrome with length l from index i
+        memset(dp,0,sizeof(dp));
+        int ans_index = 0;
+        int size = 1;
+        memset(dp[1],1,s.size()*sizeof(int)); //size 1 is palindrome
+        for(int l = 2; l <= s.size();l++){
+            for(int i = 0; i <= s.size()-l;i++){
+                //cout << l << " " << i << endl;
+                if (s[i] == s[i+l-1]){
+                    if (l == 2 ||  dp[l-2][i+1]){
+                        dp[l][i] = 1;
+                        if (l > size){
+                            ans_index = i;
+                            size = l;
+                        } // 0 1 2 
+                          // 3
+                    }
+                }
             }
         }
-        cout << center <<"    " << length << endl;
-        if (center < 0){ //a half center
-            center = (center*-1)-1;
-            return s.substr(center+1-length/2,length);
-        }
-        return s.substr(center-length/2,length);
+        return s.substr(ans_index,size);
     }
 };
