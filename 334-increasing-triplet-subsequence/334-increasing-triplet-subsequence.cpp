2e21961fc68acc2830dbc0a17e7e500f69a7e2f0
LT <56750861+tao-luke@users.noreply.github.com>
2022-05-18 23:43:44 -0400

    Time: 83 ms (66.61%), Space: 61.5 MB (71.40%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -4,11 +4,11 @@ public:
         int n1 = INT_MAX;
         int n2 = INT_MAX;
         for(const auto& n: nums){
-            if (n < n1){
+            if (n <= n1){
                 n1 = n;
-            }else if (n != n1 && n < n2){
+            }else if (n <= n2){
                 n2 = n;
-            }else if (n != n2 && n > n2){
+            }else{
                 return true;
             }
         }
