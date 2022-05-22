5d8f8e7e0d44e6ea884900101e5efc7126ae0a80
LT <56750861+tao-luke@users.noreply.github.com>
2022-05-21 21:53:28 -0400

    Time: 36 ms, Memory: 12.2 MB - LeetHub

--- number-of-islands/number-of-islands.cpp
+++ number-of-islands/number-of-islands.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    
-    void explore(vector<vector<char>>& grid,int i, int j,bool seen[][300]){
-        seen[i][j] = true;
-        
-        if ((i-1 >= 0) && (grid[i-1][j] == '1') && (!seen[i-1][j])) explore(grid,i-1,j,seen);
-        if ((i+1 < grid.size()) && (grid[i+1][j] == '1') && (!seen[i+1][j])) explore(grid,i+1,j,seen);
-        if (j-1 >= 0 && grid[i][j-1] == '1' && !seen[i][j-1]) explore(grid,i,j-1,seen);
-        if (j+1 < grid[0].size() && grid[i][j+1] == '1' && !seen[i][j+1]) explore(grid,i,j+1,seen);
-    }
     int numIslands(vector<vector<char>>& grid) {
-        //set all as 0
-        bool seen[grid.size()][300];
-        memset(seen,0,sizeof(seen));
-        
         int result = 0;
-        for(int i = 0; i < grid.size();i++){
-            for(int j = 0; j < grid[0].size();j++){
-                if (seen[i][j] == false && grid[i][j] == '1'){ //not explored and IS island
-                    explore(grid,i,j,seen);
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[0].size(); j++){
+                if (grid[i][j] == '1'){
+                    dfs(grid,i,j);
                     result++;
                 }
             }
         }
         return result;
     }
+    void dfs(vector<vector<char>>& grid, int i, int j){
+        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] != '1') return;
+        grid[i][j] = '0';
+        dfs(grid,i-1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
+        dfs(grid,i+1,j);
+    }
+    
 };
