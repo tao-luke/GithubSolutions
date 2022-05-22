d9b119b95c4b591e4ac65c62e428f61db7935ec9
LT <56750861+tao-luke@users.noreply.github.com>
2022-05-22 13:00:35 -0400

    Time: 4 ms, Memory: 11.4 MB - LeetHub

--- generate-parentheses/generate-parentheses.cpp
+++ generate-parentheses/generate-parentheses.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    void helper(vector<string>& result, string&& s ,int&& open, int && close, int && max){
-        if (open == max){
-            int tmp = close;
-            while(tmp != max){
-                s.push_back(')');
-                tmp++;
-            }
-            
-            result.push_back(s);
-            return;
-        }
-        helper(result, s + "(",open+1, move(close), move(max));
-        if (open > close){
-            helper(result, s + ")", move(open), close+1, move(max));
-        }
-    }
     vector<string> generateParenthesis(int n) {
         vector<string> result{};
-        helper(result,"",0,0,move(n));
+        string tmp {};
+        helper(n,0,0,tmp,result);
         return result;
+    }
+    void helper(int n , int open,int close ,string& tmp, vector<string>& result){
+        if (open == close && open == n){
+            result.push_back(tmp);
+            return;
+        } 
+        if (open < n){
+            tmp.push_back('(');
+            helper(n,open+1,close,tmp,result);
+            tmp.pop_back();
+        }
+        if (close < open){
+            tmp.push_back(')');
+            helper(n,open,close+1,tmp,result);
+            tmp.pop_back();
+        }
+        return;
     }
 };
