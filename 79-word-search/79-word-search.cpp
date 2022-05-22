11e89027747057c0fa4663147cae80c168c2e2a2
LT <56750861+tao-luke@users.noreply.github.com>
2022-05-22 14:24:37 -0400

    Time: 353 ms (69.49%), Space: 7.8 MB (91.39%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
+        // ok... this is stupid lmao. 1) I tried optimizing with a mapping of (vertice, char) -> bool, which is a logic error. as the direction of which we are mapping(where we are matching next matters too.) 2) used a hashmap to store the path, 2 slow... but runtime is same O()wise, mn*3^l
         for(int i = 0; i < board.size();i++){
             for(int j = 0; j < board[0].size();j++){
                 if (comp(word,0,i,j,board)) return true;
