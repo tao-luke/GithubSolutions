3cd15b6c5dfc5c2d0016ae2a4895a747a5a2088d
LT <56750861+tao-luke@users.noreply.github.com>
2022-05-31 12:00:19 -0400

    Time: 3 ms (33.11%), Space: 5.7 MB (95.09%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
-    int getSum(unsigned int a, unsigned b) {
-        if (b == 0) return a;
-        else return getSum((a^b),(a&b)<<1);
+    int getSum(unsigned int a, unsigned int b) {
+        if (b != 0){
+            return (getSum(a^b, (a&b) << 1));
+        }
+        return (int)a;
     }
 };
+
+
