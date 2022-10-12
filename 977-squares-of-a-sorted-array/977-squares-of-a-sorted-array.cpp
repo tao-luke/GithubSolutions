20b5ae51f5a3d2276af0feb9a6648a00abab52e3
LT <56750861+tao-luke@users.noreply.github.com>
2022-10-11 21:30:21 -0700

    Time: 424 ms (5.18%), Space: 26.8 MB (31.27%) - LeetHub

---
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int init_left(vector<int>& nums){ //returns the last index of negative #
+      int l = -1;
+      while((l+1 <(int) nums.size()) and nums[l+1] < 0){
+        l++;
+      }
+      return l;
+    }
+    int get_next_index_and_inc(vector<int>& nums, int& index_1, int& index_2){ //returns the index with smaller # and incr it
+      if (index_1 == -1) return index_2++;
+      if (index_2 == nums.size()) return index_1--;
+      if (-1 * nums[index_1] > nums[index_2]){
+          index_2++;
+          return index_2-1;
+      }
+      index_1--;
+      return index_1+1;
+    }
+    vector<int> sortedSquares(vector<int>& nums) {
+      if (nums.size() == 1) return vector<int>{nums[0]*nums[0]};
+        vector<int> res{};
+      int index_1 = init_left(nums);
+      int index_2 = index_1 + 1;
+      while(index_1 != -1 || index_2 != nums.size()){
+        cout << index_1 << " " << index_2 << endl;
+        int next_index = get_next_index_and_inc(nums,index_1,index_2);
+        res.push_back(nums[next_index]*nums[next_index]);
+      }
+      return res;
+    }
+  
+};
